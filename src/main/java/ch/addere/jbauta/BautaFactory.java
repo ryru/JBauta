0f2e46fb63e478090b0d6daa5f8e1e4883a599f3
Pascal K <ryru@addere.ch>
2018-03-17 16:18:16 +0100

    add support for CIDR notation bitmasks

--- src/main/java/ch/addere/jbauta/BautaFactory.java
+++ src/main/java/ch/addere/jbauta/BautaFactory.java
@@ -40,7 +40,7 @@ public class BautaFactory {
   }
 
   /**
-   * Create an IPv4 context with a non-default bitmask context.
+   * Create an IPv4 context with a non-default bitmask context on address base.
    *
    * By setting the appropriate bitmask this method allows to control every single bit of a given IPv4 to masquerade. This method allows to create own bitmask to masquerade IPv4 with.
    *
@@ -54,6 +54,46 @@ public class BautaFactory {
   public BautaV4 createIPv4(@NotNull final InetAddress maskingAddress) {
 
     return new BautaV4(maskingAddress);
+  }
+
+  /**
+   * Create an IPv4 context with a non-default bitmask context on CIDR notation base.
+   *
+   * This method allows to create own bitmask based on CIDR notation to masquerade IPv4 with.
+   *
+   * <p>Example 1: A bitmask address set to 32 will result in an address bitmask of 255.255.255.255 and therefor not modify any address while a bitmask address set to 0 will always return 0.0.0.0 regardless of the input IPv4 address.</p>
+   *
+   * <p>Example 2: A bitmask address set to 12 will only keep the first 12 bits of a given IPv4 address to mask.</p>
+   *
+   * @param slashBitmask Bitmask in CIDR notation to use for masquerading
+   * @return Instance of BautaV4 with non-default bitmask
+   */
+  public BautaV4 createIPv4(final int slashBitmask) {
+    if (slashBitmask < 0 || slashBitmask > 32) {
+      throw new IllegalArgumentException(
+          "Error: Slash should be between 0 and 32 for IP version 4 addresses");
+    }
+
+    byte[] octets = new byte[4];
+    int amountOfFullySetOctets = slashBitmask / 8;
+    int partiallySetBitsInLastOctet = slashBitmask % 8;
+
+    int i;
+    for (i = 0; i < amountOfFullySetOctets; i++) {
+      octets[i] = setByte(8);
+    }
+
+    if (partiallySetBitsInLastOctet != 0) {
+      octets[i] = setByte(partiallySetBitsInLastOctet);
+    }
+
+    try {
+      InetAddress maskingAddress = InetAddress.getByAddress(octets);
+      return new BautaV4(maskingAddress);
+    } catch (UnknownHostException e) {
+      throw new AssertionError(
+          "this should never happen: the IP address is supposed to be always valid", e);
+    }
   }
 
   /**
@@ -87,6 +127,56 @@ public class BautaFactory {
   public BautaV6 createIPv6(@NotNull final InetAddress maskingAddress) {
 
     return new BautaV6(maskingAddress);
+  }
+
+  /**
+   * Create an IPv6 context with a non-default bitmask context on CIDR notation base.
+   *
+   * This method allows to create own bitmask based on CIDR notation to masquerade IPv6 with.
+   *
+   * <p>Example 1: A bitmask address set to 128 will result in an address bitmask of FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF and therefor not modify any address while a bitmask address set to 0 will always return :: regardless of the input IPv6 address.</p>
+   *
+   * <p>Example 2: A bitmask address set to 12 will only keep the first 12 bits of a given IPv6 address to mask.</p>
+   *
+   * @param slashBitmask Bitmask in CIDR notation to use for masquerading
+   * @return Instance of BautaV4 with non-default bitmask
+   */
+  public BautaV6 createIPv6(final int slashBitmask) {
+    if (slashBitmask < 0 || slashBitmask > 128) {
+      throw new IllegalArgumentException(
+          "Error: Slash should be between 0 and 128 for IP version 6 addresses");
+    }
+
+    byte[] octets = new byte[16];
+    int amountOfFullySetOctets = slashBitmask / 8;
+    int partiallySetBitsInLastOctet = slashBitmask % 8;
+
+    int i;
+    for (i = 0; i < amountOfFullySetOctets; i++) {
+      octets[i] = setByte(8);
+    }
+
+    if (partiallySetBitsInLastOctet != 0) {
+      octets[i] = setByte(partiallySetBitsInLastOctet);
+    }
+
+    try {
+      InetAddress maskingAddress = InetAddress.getByAddress(octets);
+      return new BautaV6(maskingAddress);
+    } catch (UnknownHostException e) {
+      throw new AssertionError(
+          "this should never happen: the IP address is supposed to be always valid", e);
+    }
+  }
+
+  private byte setByte(int setBits) {
+    int newByte = 0xFF;
+
+    newByte = newByte >> setBits;
+    newByte = ~newByte;
+    newByte = newByte & 0xFF;
+
+    return (byte) newByte;
   }
 
 }
--- src/test/java/ch/addere/jbauta/BautaFactoryTest.java
+++ src/test/java/ch/addere/jbauta/BautaFactoryTest.java
@@ -5,12 +5,15 @@
 
 package ch.addere.jbauta;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.function.Executable;
 
 /**
  * JUnit tests for BautaFactory
@@ -27,6 +30,17 @@ class BautaFactoryTest {
       (byte) 0x00, (byte) 0x00, (byte) 0x00, (byte) 0x00};
   private static InetAddress ipv4Mask1;
   private static InetAddress ipv6Mask1;
+  private InetAddress fullySetv4;
+  private InetAddress fullySetv6;
+
+  {
+    try {
+      fullySetv4 = InetAddress.getByName("255.255.255.255");
+      fullySetv6 = InetAddress.getByName("FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF");
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
 
   static {
     try {
@@ -73,5 +87,373 @@ class BautaFactoryTest {
   void testCreateIPv6NonDefaultMask() {
     BautaV6 testObj = factory.createIPv6(ipv6Mask1);
     assertTrue(testObj.getClass() == BautaV6.class);
+  }
+
+  @Test
+  void testIPv4SlashBitmask1() {
+    BautaV4 bta = factory.createIPv4(0);
+    try {
+      bta.maskAny(fullySetv4);
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask2() {
+    BautaV4 bta = factory.createIPv4(32);
+    try {
+      bta.maskAny(fullySetv4);
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask3() {
+    Executable invalidIPSlashBitmask = () -> {
+      BautaV4 bta = factory.createIPv4(-1);
+      bta.maskAny(fullySetv4);
+    };
+
+    assertThrows(IllegalArgumentException.class, invalidIPSlashBitmask);
+  }
+
+  @Test
+  void testIPv4SlashBitmask4() {
+    Executable invalidIPSlashBitmask = () -> {
+      BautaV4 bta = factory.createIPv4(33);
+      bta.maskAny(fullySetv4);
+    };
+
+    assertThrows(IllegalArgumentException.class, invalidIPSlashBitmask);
+  }
+
+  @Test
+  void testIPv4SlashBitmask5() {
+    BautaV4 bta = factory.createIPv4(0);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("0.0.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask6() {
+    BautaV4 bta = factory.createIPv4(8);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.0.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask7() {
+    BautaV4 bta = factory.createIPv4(16);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.255.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask8() {
+    BautaV4 bta = factory.createIPv4(24);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.255.255.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask9() {
+    BautaV4 bta = factory.createIPv4(32);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.255.255.255");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask10() {
+    BautaV4 bta = factory.createIPv4(9);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.128.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask11() {
+    BautaV4 bta = factory.createIPv4(10);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.192.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask12() {
+    BautaV4 bta = factory.createIPv4(11);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.224.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask13() {
+    BautaV4 bta = factory.createIPv4(12);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.240.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask14() {
+    BautaV4 bta = factory.createIPv4(13);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.248.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask15() {
+    BautaV4 bta = factory.createIPv4(14);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.252.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv4SlashBitmask16() {
+    BautaV4 bta = factory.createIPv4(15);
+    try {
+      InetAddress ipv4 = InetAddress.getByName("255.255.255.255");
+      InetAddress outputShouldBe = InetAddress.getByName("255.254.0.0");
+      assertEquals(outputShouldBe, bta.maskAny(ipv4));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask1() {
+    BautaV6 bta = factory.createIPv6(0);
+    try {
+      bta.maskAny(fullySetv6);
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask2() {
+    BautaV6 bta = factory.createIPv6(32);
+    try {
+      bta.maskAny(fullySetv6);
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask3() {
+    Executable invalidIPSlashBitmask = () -> {
+      BautaV6 bta = factory.createIPv6(-1);
+      bta.maskAny(fullySetv6);
+    };
+
+    assertThrows(IllegalArgumentException.class, invalidIPSlashBitmask);
+  }
+
+  @Test
+  void testIPv6SlashBitmask4() {
+    Executable invalidIPSlashBitmask = () -> {
+      BautaV6 bta = factory.createIPv6(129);
+      bta.maskAny(fullySetv6);
+    };
+
+    assertThrows(IllegalArgumentException.class, invalidIPSlashBitmask);
+  }
+
+  @Test
+  void testIPv6SlashBitmask5() {
+    BautaV6 bta = factory.createIPv6(0);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask6() {
+    BautaV6 bta = factory.createIPv6(16);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask7() {
+    BautaV6 bta = factory.createIPv6(32);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:FFFF::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask8() {
+    BautaV6 bta = factory.createIPv6(48);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:FFFF:FFFF::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask9() {
+    BautaV6 bta = factory.createIPv6(128);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = fullySetv6;
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask10() {
+    BautaV6 bta = factory.createIPv6(17);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:8000::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask11() {
+    BautaV6 bta = factory.createIPv6(18);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:C000::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask12() {
+    BautaV6 bta = factory.createIPv6(19);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:E000::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask13() {
+    BautaV6 bta = factory.createIPv6(20);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:F000::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask14() {
+    BautaV6 bta = factory.createIPv6(21);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:F800::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask15() {
+    BautaV6 bta = factory.createIPv6(22);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:FC00::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
+  }
+
+  @Test
+  void testIPv6SlashBitmask16() {
+    BautaV6 bta = factory.createIPv6(23);
+    try {
+      InetAddress ipv6 = fullySetv6;
+      InetAddress outputShouldBe = InetAddress.getByName("FFFF:FE00::");
+      assertEquals(outputShouldBe, bta.maskAny(ipv6));
+    } catch (UnknownHostException e) {
+      e.printStackTrace();
+    }
   }
 }
